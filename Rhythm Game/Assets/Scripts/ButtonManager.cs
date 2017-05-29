16ffec1afb5ab23b3bb1094f50d53fb3babd6e8d
Joshua Blair <jblair19@uncc.edu>
2017-05-29 16:09:17 -0400

    Added Title screen and new sky boxes
    Title screen will load main game scene when you hit start. Quit button partially implemented and tutorial button will be implemented later but they are both clickable. Multiple new skyboxes added in textures folder though they probably shouldn't go there.

---
+++ Rhythm Game/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour {
+
+	public void StartGameBtn(string newGameLevel)
+	{
+		SceneManager.LoadScene (newGameLevel);
+	}
+
+	public void StartTutorialBtn(string newGameLevel)
+	{
+		
+	}
+
+	public void ExitGameBtn(string newGameLevel)
+	{
+		Application.Quit ();
+	}
+}
